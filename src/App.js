201b00f17881d52aa53f8802e5837c8c8598050f
Tien <viettienha98@gmail.com>
2021-01-29 14:10:47 +0700

    Using env

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is alpha release. Stay home stay safe.
+          VietNam quyet tam chien thang dai dich Covid. <br />
+          Key: {process.env.REACT_APP_KEY}
         </p>
         <a
           className="App-link"
